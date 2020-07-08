c24d542ca08c7789ca88aaf84c9a0a81cbb65819
dans-acc <danv123456789@gmail.com>
2020-07-08 17:44:42 +0100

    Updating everything

--- setup.py
+++ setup.py
@@ -4,9 +4,7 @@ setup(
     name='EEGLearn',
     version='1.11',
     packages=['eeglearn'],
-    install_requires=['numpy==1.13.1', 'scipy==0.19.1', 'scikit-learn==0.18.2', 'theano==0.8',
-                      'lasagne @ git+https://github.com/Lasagne/Lasagne.git#egg=lasagne=0.2.dev1'],
-    url='https://github.com/pbashivan/EEGLearn',
+    url='https://github.com/dans-acc/EEGLearn',
     license='GNU GENERAL PUBLIC LICENSE',
     author='Pouya Bashivan',
     description='Representation learning from EEG'
